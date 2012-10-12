af0cf74ef9f8cefb86d69a05f9855c42ac37c7f9
Kenneth Calamay <dev+kenneth@proudcloud.net>
2012-10-13 00:50:10 +0800

    Added seed data for referrers

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,10 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 # Committees
-["Mission", "Volunteers", "Materials", "Marketing", "Finance"].sort!.each do |c|
-  Committee.create!(:name => c)
-end
+["Mission", "Volunteers", "Materials", "Marketing", "Finance"].sort!.each do |name|
+  Committee.create!(:name => name)
+end if Committee.all.blank?
+
+["Colleague","Seminar","Facebook", "Can't remember", "Others"].sort!.each do |source|
+  Referrer.create!(:source => source)
+end if Referrer.all.blank?
